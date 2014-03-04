3fa1c784f59b51361b201b1ae3c04fde951ed46b
Burke Holland <burkeholland@gmail.com>
2014-03-04 09:01:06 -0600

    initial commit

---
+++ KendoUISpaTemplateVsix/ProjectTemplates/KendoUISpaProjectTemplate.cshtml/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace $safeprojectname$ {
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles) {
+            bundles.IgnoreList.Clear();
+            AddDefaultIgnorePatterns(bundles.IgnoreList);
+
+            bundles.Add(
+              new ScriptBundle("~/scripts/vendor")
+                .Include("~/scripts/jquery-{version}.js")
+                .Include("~/scripts/pubsub.js")
+                .Include("~/scripts/bootstrap.js")
+                .Include("~/scripts/debug.js")
+              );
+
+            bundles.Add(
+              new StyleBundle("~/Content/css")
+                .Include("~/Content/kendo/2013.3.1119/kendo.common-bootstrap.min.css")
+                .Include("~/Content/kendo/2013.3.1119/kendo.bootstrap.min.css")
+                .Include("~/Content/bootstrap.css")
+                .Include("~/Content/font-awesome.css")
+              );
+        }
+
+        public static void AddDefaultIgnorePatterns(IgnoreList ignoreList) {
+            if (ignoreList == null) {
+                throw new ArgumentNullException("ignoreList");
+            }
+
+            ignoreList.Ignore("*.intellisense.js");
+            ignoreList.Ignore("*-vsdoc.js");
+
+            //ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+            //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+        }
+    }
+}
---
+++ KendoUISpaTemplateVsix/ProjectTemplates/KendoUISpaProjectTemplate.cshtml/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using System.Web;
+using System.Web.Mvc;
+
+namespace $safeprojectname$.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
